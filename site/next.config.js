db91e5e185bf667459eecd02e61741d7263c86b6
Josh <josh.walters@outlook.com>
2024-03-16 15:36:41 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Wal7er5 Registry',
+    description: 'My list of Kasm workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://walts81.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/walts81/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
